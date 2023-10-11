1d9f465ffdf0cb9b20a194ca24b8c99525193660
Pratham <prathamnaveen.m@gmail.com>
2023-10-11 23:00:17 +0530

    Keeper Project Part-1

---
+++ Keeper App/src/components/Note.jsx
@@ -0,0 +1,14 @@
+// Note Component
+
+import React from "react";
+
+function Note(){
+    return (
+        <div className="note">
+            <h1>Note for Title</h1>
+            <p>Note for Content</p>
+        </div>
+    );
+}
+
+export default Note;
---
+++ Keeper App/src/index.js
@@ -0,0 +1,8 @@
+// Main
+// Install the dependencies before running it
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from "./components/App";
+
+ReactDOM.render(<App/>, document.getElementById("root"));
